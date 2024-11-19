742dc940b3814f84a0d0548c56884ba2c5756285
viniciusibiapina007 <viniciusaluno2020@gmail.com>
2024-11-19 23:05:59 +0000

    dddd

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main()
            for(int c = 0; c < col; c++){
                printf("%d", matriz[l][c]);
                
-               if(j<col-1){
+               if(c<col-1){
                 printf(" ";)
                }
             }
