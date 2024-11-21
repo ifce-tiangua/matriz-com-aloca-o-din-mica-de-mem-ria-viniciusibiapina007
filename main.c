adc0e842cf69ef6a2005f1d9b7a33db3e025b1fe
viniciusibiapina007 <viniciusaluno2020@gmail.com>
2024-11-21 00:56:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int lin, col;
+int main() {
+	int lin, col;
 	scanf("%d", &lin);
 	scanf("%d", &col);
 
 	int **matriz = (int**) malloc(lin * sizeof(int*));
-	
-	if(lin == 0 || col == 0){ // se a linha ou a coluna é for 0, a matriz é nula
+	if(lin == 0 || col == 0) {
 		matriz[0] = NULL;
 	}
 	else {
-		for(int l = 0; l < lin; l++) {
-			matriz[l] = (int*) malloc(col * sizeof(int));
+		for(int i = 0; i < lin; i++) {
+			matriz[i] = (int*) malloc(col * sizeof(int));
 		}
 	}
-
 	if(matriz[0] != NULL) {
-		for(int l = 0; l < lin; l++) {
-			for(int c = 0; c < col; c++) {
-				scanf("%d", &matriz[l][c]);
+		for(int i = 0; i < lin; i++) {
+			for(int j = 0; j < col; j++) {
+				scanf("%d", &matriz[i][j]);
 			}
 		}
-
-		for(int l = 0; l < lin; l++) {
-			for(int c = 0; c < col; c++) {
-				if(c == col-1) {
-					printf("%d", matriz[l][c]);
+		for(int i = 0; i < lin; i++) {
+			for(int j = 0; j < col; j++) {
+				if(j == col-1) {
+					printf("%d", matriz[i][j]);
 				}else{
-					printf("%d ", matriz[l][c]);
+					printf("%d ", matriz[i][j]);
 				}
 			}
 			printf("\n");
@@ -39,10 +35,9 @@ int main()
 		printf("[matriz vazia]\n");
 	}
 
-	for(int l = 0; l < lin; l++) {
-		free(matriz[l]); // desalocar cada linha
+	for(int i = 0; i < lin; i++) {
+		free(matriz[i]);
 	}
-	free(matriz); // desalocar a matriz por completo
-    
-    return 0;
+	free(matriz);
+	return 0;
 }
